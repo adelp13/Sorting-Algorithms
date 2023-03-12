0b0791d40585d8c4caf3184c3eb7c91ec295613f
adelp13 <adela.petre13@gmail.com>
2023-03-12 23:46:01 +0200

    .
    .

--- proiect1SD/quicksort.cpp
+++ proiect1SD/quicksort.cpp
@@ -1,36 +1,85 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
-int v[500001], N;
+int v[100000001], N;
 
-void quickSort(int v[500001], int left, int right){
+void quickSortRandom(int v[100000001], int left, int right){
     if (left < right)
     {
-        int mid = (left + right)/2;
-        int val = v[mid];
+        int index = left + (rand() % (right - left +1));
+        int val = v[index];
         int above = 0;
-        cout << 'x';
+        int indexVal;
+
         for (int i = left; i <= right - above; i++)
         {
-            if (v[i] > val)
+            if (v[i] >= val)
             {
-                above++;
+                if(v[i] == val)
+                    indexVal = right - above;
                 swap(v[i], v[right - above]);
+                above++;
                 i--;
             }
         }
-        quickSort(v, left, right - above - 1);
-        quickSort(v, right - above , right);
+        swap(v[right - above + 1], v[indexVal]);
+        quickSortRandom(v, left, right - above);
+        quickSortRandom(v, right - above + 2, right);
+    }
+    return;
+}
+
+void quickSortMedian3(int v[100000001], int left, int right){
+    if (left < right)
+    {
+        int mid = (left + right) / 2;
+        if (v[left] > v[mid])
+            swap(v[left], v[mid]);
+        if (v[mid] > v[right])
+            swap(v[right], v[mid]);
+        if (v[left] > v[right])
+            swap(v[right], v[left]);
+        int val = v[mid];
+        int above = 0, indexVal;
+
+        for (int i = left; i <= right - above; i++)
+        {
+            if (v[i] >= val)
+            {
+                if(v[i] == val)
+                    indexVal = right - above;
+                swap(v[i], v[right - above]);
+                above++;
+                i--;
+            }
+        }
+        swap(v[right - above + 1], v[indexVal]);
+        quickSortMedian3(v, left, right - above);
+        quickSortMedian3(v, right - above + 2, right);
     }
     return;
 }
 int main()
 {
-    cin >> N;
+    /*cin >> N;
     for (int i = 1; i <= N; i++)
         cin >> v[i];
-    quickSort(v, 1, N);
-    for (int i = 1; i <= N; i++)
-        cout << v[i] << ' ';
+        */
+    for (int i = 1; i <= 100000000; i++)
+        v[i] = rand()%1000000;
+    cout << "da";
+    quickSortMedian3(v, 1, 100000000);
+    bool da = true;
+    for (int i = 1; i < 100000000; i++)
+    if (v[i] > v[i + 1])
+    {
+        da = false;
+        break;
+    }
+    cout << "da";
+    cout << da;
+    //for (int i = 1; i <= 100000; i++)
+        //cout << v[i] << ' ';
     return 0;
 }
--- proiect1SD/shellSort.cpp
+++ proiect1SD/shellSort.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
+#include <cstdlib>
 #include <fstream>
 using namespace std;
 
 ifstream fin("nr.txt");
-int v[500001], N;
-void shellSort(int v[500001], int h, int N)
+int v[100000001], N = 100000000;
+void shellSort(int v[100000001], int h, int N)
 {
     if (h >= 1)
     {
         for (int i = 1; i <= h; i++)
-            for (int j = i + h; j <= N; j++)
+            for (int j = i + h; j <= N; j += h)
             {
                 int k = j - h;
                 while(k >= i && v[k] > v[k + h])
@@ -23,12 +24,28 @@ void shellSort(int v[500001], int h, int N)
 }
 int main()
 {
-    fin >> N;
+    /*fin >> N;
     for (int i = 1; i <= N; i++)
         fin >> v[i];
     shellSort(v, N/2, N);
     for (int i = 1; i <= N; i++)
         cout << v[i] << ' ';
+        */
+    for (int i = 1; i <= N; i++)
+        v[i] = rand()%1000000;
+    cout << "da";
+    shellSort(v, N/2, N);
+    bool da = true;
+    for (int i = 1; i < N; i++)
+    if (v[i] > v[i + 1])
+    {
+        da = false;
+        break;
+    }
+    cout << "da";
+    cout << da;
+    //for (int i = 1; i <= 100000; i++)
+        //cout << v[i] << ' ';
     return 0;
 }
 
